69f009176748d0f505baff9f821ab14d8d999667
Jason Chen <superj63@hotmail.com>
2019-12-26 10:30:23 +0800

    skillz

--- src/components/sections/skills.js
+++ src/components/sections/skills.js
@@ -1,17 +1,33 @@
 import React from 'react'
 
-import "../../styles/skills.css"
-import { Container } from 'react-bootstrap'
+import "../../styles/skills.scss"
+import { Container, Row, Col } from 'react-bootstrap'
 
 const Skills = () => {
+  const languages = ["C++", "Java", "Python", "Javascript (ES6)", "SQL"]
+  const frameworks = ["Pandas", "Numpy", "Keras", "Tensorflow", "React", "Node", "Java Servlets"]
+  const tools = ["Bash", "Git & Github", "MySQL", "GCP", "AWS"]
   return (
     <Container fluid={true} id="skills" className="skills-main">
-      <div className="heading">
-        <h4>Skills</h4>
-        <div className= "divider"></div>
-      </div>
-      <div className="skills-desc">
-        
+      <div className="skills-chunk">
+        <div classname="skills-category" lg={3}>
+          <ul className="skills-list">
+            <li className="title">Languages</li>
+            {languages.map((i) => <li className="skills-category-item">{i}</li>)}
+          </ul>
+        </div>
+        <div classname="skills-category" lg={3}>
+          <ul className="skills-list">
+            <li className="title">Frameworks</li>
+            {frameworks.map((i) => <li className="skills-category-item">{i}</li>)}
+          </ul>
+        </div>
+        <div classname="skills-category" lg={3}>
+          <ul className="skills-list">
+            <li className="title">Tools</li>
+            {tools.map((i) => <li className="skills-category-item">{i}</li>)}
+          </ul>
+        </div>
       </div>
       
     </Container>
